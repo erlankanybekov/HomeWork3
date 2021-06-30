c15d1d0ea703a44564674c33685a21a48f7b4896
erlankanybekov <86570752+erlankanybekov@users.noreply.github.com>
2021-06-30 18:39:10 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	String [] names = new String[3];
-	names [0] = "Erlan";
-	names [1] = "Beka";
-	names [2] = "Mirlan";
+        String [] names = new String[3];
+        names [0] = "Erlan";
+        names [1] = "Beka";
+        names [2] = "Mirlan";
         for (String newname:names)
             switch (newname){
                 case "Erlan":
@@ -17,9 +17,10 @@ public class Main {
                     break;
                 case "Mirlan":
                     System.out.println("Добрый вечер "+ names[2]);
-                    break;
 
+                default:
+                    System.out.println("Здравствуйте " +  names[0]+ " , " + names[1] +" , " + names[2]);
             }
 
-        }
     }
+}
