f408c3a0da63dbd4645ab845cb85d7f580c7557c
Aiperi Faisova <alice.faisova@gmail.com>
2021-06-30 18:21:18 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String [] names = new String[3];
+	names [0] = "Erlan";
+	names [1] = "Beka";
+	names [2] = "Mirlan";
+        for (String newname:names)
+            switch (newname){
+                case "Erlan":
+                    System.out.println("Доброе утро "+ names[0]);
+                    break;
+                case "Beka":
+                    System.out.println("Добрый день "+ names[1]);
+                    break;
+                case "Mirlan":
+                    System.out.println("Добрый вечер "+ names[2]);
+                    break;
+
+            }
+
+        }
+    }
